0f44f21d43b65c59a168d26c54229b92a4b6b08c
aumizy <aumizy01@gmail.com>
2024-05-09 23:17:06 -0700

    6-peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-""" a function that finds a peak in a list of unsorted integers """
+""" Create find_peak function """
 
 
 def find_peak(list_of_integers):
-    """ This function returns the peak of the list """
-    if (len(list_of_integers) == 0):
+    """
+    Find a peak in a list of unsorted integers.
+    """
+    if not list_of_integers:
         return None
 
+    return find_peak_recursive(list_of_integers, 0, len(list_of_integers) - 1)
+
+
+def find_peak_recursive(arr, low, high):
+    """
+    Find a peak recursion.
+    """
+    if low == high:
+        return arr[low]
+
+    mid = (low + high) // 2
+
+    if arr[mid] > arr[mid + 1]:
+        return find_peak_recursive(arr, low, mid)
     else:
-        top = list_of_integers[0]
-        for i in range(len(list_of_integers)):
-            if list_of_integers[i] > top:
-                top = list_of_integers[i]
-        return top
+        return find_peak_recursive(arr, mid + 1, high)
