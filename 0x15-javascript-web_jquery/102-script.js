45225f81a48c0ae9cc94246471ace99fd9d07017
aumizy <aumizy01@gmail.com>
2024-06-11 12:32:35 -0700

    all files

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#btn_translate').click(function() {
+        var languageCode = $('#language_code').val();
+        $.ajax({
+            url: 'https://hellosalut.stefanbohacek.dev/?lang=${lang}',
+            method: 'GET',
+            data: { lang: languageCode },
+            dataType: 'json',
+            success: function(response) {
+                var translation = response.hello;
+                console.log("Translation:", translation);
+                $('#hello').text(translation);
+            }
+        });
+    });
+});
